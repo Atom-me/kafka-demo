86b89d14a7f02d44bdee864b7c4c8ddc20172386
Atom <sarming@126.com>
2021-10-26 17:00:17 +0800

    post kafka message

--- springboot-kafka/src/main/java/com/atom/kafka/controller/KafkaController.java
+++ springboot-kafka/src/main/java/com/atom/kafka/controller/KafkaController.java
@@ -2,13 +2,9 @@ package com.atom.kafka.controller;
 
 import com.atom.kafka.KafkaMessage;
 import com.atom.kafka.KafkaProducer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-
 import java.util.Date;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -23,6 +19,14 @@ public class KafkaController {
     @Resource
     private KafkaProducer kafkaProducer;
 
+    /**
+     * get request publish kafka message.
+     *
+     * @param id
+     * @param username
+     * @param password
+     * @return
+     */
     @RequestMapping(value = "/message", method = RequestMethod.GET)
     public KafkaMessage sendKafkaMessage(@RequestParam(name = "id") Long id,
                                          @RequestParam(name = "username") String username,
@@ -36,6 +40,24 @@ public class KafkaController {
 
         this.kafkaProducer.sendKafkaMessage(kafkaMessage);
 
+        return kafkaMessage;
+    }
+
+
+    /**
+     * curl -X POST -H "Content-Type: application/json" -d '{"id":5,"username":"lisi","password":"2312432"}' 'http://localhost:8080/kafka/message2'
+     * <p>
+     * post request publish kafka message.
+     *
+     * @param kafkaMessage
+     * @return
+     */
+    @RequestMapping(value = "/message2", method = RequestMethod.POST)
+    public KafkaMessage sendKafkaMessage2(@RequestBody KafkaMessage kafkaMessage) {
+        System.out.println("sendKafkaMessage2 invoked!");
+        kafkaMessage.setDate(new Date());
+
+        this.kafkaProducer.sendKafkaMessage(kafkaMessage);
         return kafkaMessage;
     }
 
